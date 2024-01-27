e0a7bb75d601a6fd499cc1254377700c028cea74
Rene Lopez <renx99@gmail.com>
2024-01-27 10:22:50 -0500

    end of day friday

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,10 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -300.0
+const WHOOPIE = 2.2
+var has_whoopie = false 
+var touching_door = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -16,14 +19,32 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+		if Input.is_action_pressed("WHOOPIE") and is_on_floor() and has_whoopie:
+			velocity.y = velocity.y * WHOOPIE
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
+		if Input.is_action_pressed("WHOOPIE") and is_on_floor() and has_whoopie:
+			velocity.x = velocity.x * WHOOPIE
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-
 	move_and_slide()
+	
+	# box pushing logic
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i) as KinematicCollision2D
+		print("i'm touching::=", collision.get_collider().name)
+		if collision.get_collider().name == "movable_box":
+			#print("i'm touching::=", collision)
+			collision.get_collider().apply_central_impulse(Vector2(direction * 7, 0))
+			
+	# open door logic
+	if Input.is_action_pressed("Open Door") and touching_door:
+		has_whoopie = true
+	
+func is_touching_door(status:bool):
+	touching_door = status 
+	
